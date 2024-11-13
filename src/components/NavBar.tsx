106f2ad92dc90fba0fdee1b1e855d310451b88c5
unknown <nrmwnalsa@gmail.com>
2024-11-13 14:52:35 +0800

    add navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,33 +1,28 @@
-//masih tes
 import React from 'react';
 import {
-    IonHeader,
-    IonToolbar, 
-    IonButtons, 
-    IonTitle, 
-    IonButton, 
-    IonIcon 
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonButtons,
+  IonButton,
+  IonIcon,
 } from '@ionic/react';
-import { 
-    personCircleOutline 
-} from 'ionicons/icons';
-import { useLocation } from 'react-router-dom';
-import './Navbar.css';
+import { personCircleOutline } from 'ionicons/icons';
+import './NavBar.css';
 
-const Navbar: React.FC = () => {
+const NavBar: React.FC = () => {
   return (
     <IonHeader>
-      <IonToolbar color="medium">
-        <IonTitle className="navbar-title">Ayomi</IonTitle>
-        <IonButtons slot="end">
-          <IonButton>
-            Events
-          </IonButton>
-          <IonButton>
-            About Us
-          </IonButton>
-          <IonButton>
-            <IonIcon icon={personCircleOutline} />
+      <IonToolbar className="navbar-background">
+        <IonTitle slot="start" className="navbar-title">
+          Ayom<i className="icon-style fa fa-trash"></i>
+        </IonTitle>
+
+        <IonButtons slot="end" className="navbar-end-buttons">
+          <IonButton className="navbar-button">Events</IonButton>
+          <IonButton className="navbar-button">About Us</IonButton>
+          <IonButton className="navbar-button">
+            <IonIcon icon={personCircleOutline} className="icon-style" />
           </IonButton>
         </IonButtons>
       </IonToolbar>
@@ -35,4 +30,4 @@ const Navbar: React.FC = () => {
   );
 };
 
-export default Navbar;
+export default NavBar;
