010f1b4e95ee2d5cead687b22a721353a8226643
Nama Anda <email@example.com>
2024-11-14 19:31:17 +0800

    perbaikan

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,33 @@
-import { IonApp, IonRouterOutlet, IonSplitPane, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { Redirect, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
-import AboutUs from './pages/AboutUs/AboutUs';
-import LandingPage from './pages/LandingPage/LandingPage';
-import Signin from './pages/Auth/Signin';
-import KerajinanSampah from './pages/KerajinanSampah/KerajinanSampah';
-
+import {
+  IonApp,
+  IonRouterOutlet,
+  IonSplitPane,
+  setupIonicReact,
+} from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import { Redirect, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import AboutUs from "./pages/AboutUs/AboutUs";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import Signin from "./pages/Auth/Signin";
+import KerajinanSampah from "./pages/KerajinanSampah/KerajinanSampah";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /**
  * Ionic Dark Mode
@@ -34,27 +38,26 @@ import '@ionic/react/css/display.css';
 
 /* import '@ionic/react/css/palettes/dark.always.css'; */
 /* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
+import "@ionic/react/css/palettes/dark.system.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
 
 setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
-      <NavBar/>
+      <NavBar />
       <IonRouterOutlet>
-        
         <Route exact path="/signIn">
           <Signin />
         </Route>
 
         <Route exact path="/LandingPage">
           <LandingPage />
-         </Route>
-        
+        </Route>
+
         <Route exact path="/AboutUs">
           <AboutUs />
         </Route>
@@ -68,9 +71,7 @@ const App: React.FC = () => (
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
-    <Footer/>
   </IonApp>
 );
-
 
 export default App;
